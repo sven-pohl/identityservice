8a62e4d2f21284abfbee8d59ae3a1c83a39bc738
Sven Pohl <sven.pohl@microsoft.com>
2020-07-30 10:58:03 +0200

    Add console Client

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        private static async Task Main()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if(disco.IsError) {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+        }
+    }
+}
