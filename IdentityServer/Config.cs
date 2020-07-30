1768c35161a362ac7f889d5925819fe2a4319228
Sven Pohl <sven.pohl@microsoft.com>
2020-07-30 15:51:18 +0200

    Add MVCClient

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -10,9 +11,9 @@ namespace IdentityServer
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
+            new List<IdentityResource>{
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -29,6 +30,25 @@ namespace IdentityServer
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = { "api1" }
+                },
+                new Client {
+                    ClientId = "mvc",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+                    
+                    // where to redirect to after login
+            RedirectUris = { "https://localhost:5000/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5000/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
+            }
                 }
             };
     }
